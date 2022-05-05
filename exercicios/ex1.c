36879dc8b18421bb0ef11c1000aa5fb88b4a567b
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-05-05 16:57:03 -0300

    Update dia1

--- exercicios/ex1.c
+++ exercicios/ex1.c
@@ -1,7 +1,30 @@
 /*
-Faça um programa que preencha um vetor com 20 números inteiros e gere
-como saída:
+Faca um programa que preencha um vetor com 20 numeros inteiros e gere
+como solucao:
 a) O vetor preenchido
-b) O maior número
-c) O menor número
+b) O maior numero
+c) O menor numero
 */
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(void){
+	int i,vetor[20];
+	
+	// Entrada de dados
+	printf("Digite 20 numeros inteiros");
+	scanf("%i",&vetor[0]);
+	
+	// Exibindo os valores do vetor
+	for (i= 0; i<=20; i++){
+		printf("Vetor[%d] = %d\n",i,vetor[i]);
+	}
+	//O maior numero
+	printf("O maior numero");
+	//O menor numero
+	printf("O menor numero");
+  
+  getch();
+  return 0;
+}
